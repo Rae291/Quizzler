544ae286ee7c61f99964cbf86803fc00f50cfde0
Rae291 <107467793+Rae291@users.noreply.github.com>
2022-12-24 18:40:44 +0530

    Add files via upload

---
+++ Quizzler/Controller/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Quizzler
+//
+//  Created by APPLE on 19/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    @IBOutlet weak var choice3Button: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var quizBrain = QuizBrain()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+    }
+
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+        //const to store user's answer
+        let userAnswer = sender.currentTitle!
+        
+        let userGotItRight = quizBrain.checkAnswer(userAnswer)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else{
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+    }
+
+    //func to change the questions
+    @objc func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        
+        let answerChoices = quizBrain.getAnswers()
+        choice1Button.setTitle(answerChoices[0], for: .normal)
+        choice2Button.setTitle(answerChoices[1], for: .normal)
+        choice3Button.setTitle(answerChoices[2], for: .normal)
+        
+        progressBar.progress = quizBrain.getProgress()
+        scoreLabel.text = "Score: \(quizBrain.getScore())"
+        choice1Button.backgroundColor = UIColor.clear
+        choice2Button.backgroundColor = UIColor.clear
+        choice3Button.backgroundColor = UIColor.clear
+        
+    }
+}
---
+++ Quizzler/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by APPLE on 19/12/22.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answers: [String]
+    let rightAnswer: String
+    
+    init(q: String, a: [String], correctAnswer: String) {
+        text = q
+        answers = a
+        rightAnswer = correctAnswer
+    }
+}
